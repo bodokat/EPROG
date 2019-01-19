910ea83adb286e87aafd46f69ba9e268e44a7a46
bodokat <bod.kato@gmail.com>
2019-01-19 13:34:13 +0100

    added iVector

---
+++ serie11/iVector.hpp
@@ -0,0 +1,16 @@
+#ifndef _IVECTOR_
+#define _IVECTOR_
+
+class iVector {
+private:
+  int len;
+  int* coeff;
+
+public:
+  iVector ();
+  iVector (const iVector& orig);
+  ~iVector ();
+  const iVector& operator=(const )
+};
+
+#endif /* end of include guard: _IVECTOR_ */
